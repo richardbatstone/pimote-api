504a9ef8e841dd42ce60454efdc5934aab693d91
richardbatstone <36573392+richardbatstone@users.noreply.github.com>
2020-11-14 22:13:47 +0000

    socket 2 control

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, json, request
 
 sockets = {
-    'socket_1': True,
+    'socket_1': False,
     'socket_2': False
 }
 
@@ -9,17 +9,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "<h1>Hello, World!</h1>"
+	return "<h1>Pimote api</h1>"
 
 @app.route('/sockets/', methods=['POST'])
 def set_socket_values():
-    if not request.json or not 'socket_1' in request.json:
-        return jsonify({'error':'error 1 set a socket'}), 400
-    if type(request.json['socket_1']) == bool:
-        sockets['socket_1'] = request.json['socket_1']
-        return jsonify({'message': 'set socket'}), 201
+    if not request.json:
+        return jsonify({'error':'error 1, send json'}), 400
+    if 'socket_1' and 'socket_2' in request.json:
+        if type(request.json['socket_1']) == bool and type(request.json['socket_2']) == bool:
+            sockets['socket_1'] = request.json['socket_1']
+            sockets['socket_2'] = request.json['socket_2']
+            return jsonify({'message': 'sockets set'}), 201
+        else:
+            return jsonify({'error':'set booleans'}), 400
     else:
-        return jsonify({'error':'error 2 set a socket'}), 400
+        return jsonify({'error':'set sockets'}), 400
 
 @app.route('/sockets/', methods=['GET'])
 def get_socket_values():
